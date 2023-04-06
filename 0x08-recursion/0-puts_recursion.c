7411399580aa0727b8305be08d84fcdfcd3b16eb
leone-nyaga <leonenyaga@gmail.com>
2023-04-06 11:08:43 -0700

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+ * _puts_recursion - print a string
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		putchar('\n');
+
+	}
+}
+
+
+}
