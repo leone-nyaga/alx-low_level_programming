d16d53087b046c24fc486939e083cbaa4588985b
leone-nyaga <leonenyaga@gmail.com>
2023-04-06 12:52:56 -0700

    Inception. Is it possible?

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * int _sqrt_recursion - returns the natural square root of a number
+ * @n: number that returns the square root
+ * Return: natural square root or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+		return (check(1, n));
+	}
+
+/**
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
+ * Return: int
+ */
+int check(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
+
