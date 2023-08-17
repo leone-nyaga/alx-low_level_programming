11eb7bfc015ee26c2a24ddb5514298577918f196
leone-nyaga <leonenyaga@gmail.com>
2023-08-17 11:14:16 -0700

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count;
+size_t count;
 
-    for (count = 0; h != NULL; h = h->next, count++)
-    {
-        printf("%d\n", h->n);
-    }
+for (count = 0; h != NULL; h = h->next, count++)
+{
+printf("%d\n", h->n);
+}
 
-    return count;
+return (count);
 }
 
