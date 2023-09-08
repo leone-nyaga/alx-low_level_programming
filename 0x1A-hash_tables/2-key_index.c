fc143abab415a2db96bf383ca2a2292665feac0e
leone-nyaga <leonenyaga@gmail.com>
2023-09-08 11:57:50 -0700

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -11,6 +11,8 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_index;
+
 	hash_index = hash_djb2(key) % size;
+
 	return (hash_index);
 }
